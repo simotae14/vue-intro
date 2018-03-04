3aee36758b5027b95e042e646c6d60bf79660d33
Simone Taeggi <startae14@gmail.com>
2018-03-04 14:51:42 +0100

    Creato un Component Globale
    creo il Component User.vue inserito globalmente nell'app

---
+++ src/components/User.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="user">
+    <h3>L'User</h3>
+    <p>Il mio username è {{ username}}</p>    
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+        username: 'Simone'
+    }
+  }
+}
+</script>
+
+<style>
+    p {
+        font-style: italic;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import User from './components/User.vue'
+
+// creo il Component
+Vue.component('app-user', User);
 
 new Vue({
   el: '#app',
